9f523a54a1185f9f9c685abc9f2810ad97167c0c
Fuann-Kinoko <838832306@qq.com>
2024-05-31 09:57:18 +0800

    add solo-jazz theme

--- packages.el
+++ packages.el
@@ -69,6 +69,7 @@
 (package! citre)
 (package! calibredb)
 
+(package! solo-jazz-theme)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
