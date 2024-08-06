b265489bd7023304f4470c4351d872ee0746b4d1
Fuann-Kinoko <838832306@qq.com>
2024-08-06 14:40:34 +0800

    disable dim when switching buffers

--- configs/module-packages/my-dimmer.el
+++ configs/module-packages/my-dimmer.el
@@ -27,9 +27,23 @@
                     0.4)))
 
 (defun my/never-dim-speical-buffers (buffer)
-  "Return non-nil if BUFFER's name starts with * and ends with *.
-   Example: '*Minibuf-1*'"
+  "Return non-nil if the name of BUFFER starts with * and ends with *.
+Example: '*Minibuf-1*' or '*info*'"
   (string-match-p ".*\\*.*\\*$" (buffer-name buffer)))
+
+(defun my/adob--remap-faces-advice (buffer object)
+  "Advice to modify `wants` in `adob--remap-faces`.
+Diasble dim for other buffers if current one is minibuffer."
+  (let ((wants (and (not (adob--never-dim-p buffer))
+                     (not (window-minibuffer-p))))
+         (has (buffer-local-value 'adob--face-mode-remapping buffer)))
+    (when (eq wants (not has))
+      (set-buffer buffer)
+      (if wants
+          (adob--remap-add-relative)
+        (adob--remap-remove-relative))
+      (force-window-update object)
+      wants)))
 
 (use-package! auto-dim-other-buffers
   :hook
@@ -39,23 +53,8 @@
   (auto-dim-other-buffers-dim-on-focus-out nil)
   (auto-dim-other-buffers-dim-on-switch-to-minibuffer nil)
   :config
-  (add-hook 'auto-dim-other-buffers-never-dim-buffer-functions 'my/never-dim-speical-buffers))
-
-;; below are for debugging
-;; (mapcar (lambda (buffer)
-;;         ;; It’s tempting to read the value of the variable and not bother
-;;         ;; with the buffer if the value is nil since in that case the
-;;         ;; buffer is presumably never-dim and thus we won’t remap any
-;;         ;; faces in it.  There is one corner case when this is not true
-;;         ;; however.  If at one point user set list of faces to affect to
-;;         ;; nil the list of remapping will be nil as well and when user
-;;         ;; changes the variable we’ll need to add remappings.
-;;         (message
-;;          (buffer-name buffer)
-;;          ;; (my/never-dim-which-key-buffer buffer)
-;;          ;; (run-hook-with-args-until-success
-;;          ;;  'auto-dim-other-buffers-never-dim-buffer-functions buffer)
-;;          ))
-;;       (buffer-list))
+  (add-hook 'auto-dim-other-buffers-never-dim-buffer-functions 'my/never-dim-speical-buffers)
+  (advice-add 'adob--remap-faces :override #'my/adob--remap-faces-advice)
+  )
 
 (provide 'my-dimmer)
